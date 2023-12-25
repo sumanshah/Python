febc27756fe3bc4df17f1905713d4d9707b224ee
sumanshah <sumanshah589@gmail.com>
2023-12-25 12:32:55 +0530

    added new programs

---
+++ Factorial(forLoop).py
@@ -0,0 +1,6 @@
+N = int(input())
+product = 1
+for i in range(1, N+1):
+    product = product * i 
+
+print(product)
---
+++ SumOfCubes.py
@@ -0,0 +1,6 @@
+N = int(input())
+sum = 0
+for i in range(1, N+1):
+    product = i ** 3
+    sum = sum + product
+print(sum)
